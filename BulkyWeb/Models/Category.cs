40f430b4cb0fec12d64a98af62040b9da9720cbe
Muhammad Ahmed <muhammad.a.esmael@gmail.com>
2023-05-22 02:02:03 +0300

    Initializing Database

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        public int Id{ get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+        
+    }
+}
